4f0b74f749ac473bdb99ac1d546b105d9ee1de01
Kegan Maher <kegan.maher@gmail.com>
2020-07-21 10:22:20 +0000

    directory and file helpers

--- src/output.js
+++ src/output.js
@@ -8,19 +8,26 @@ const turf = require("@turf/helpers");
 const { Agency } = require("./agency");
 
 /**
- * Return the path to the data output directory for this agency.
+ * Return the path to the data output directory for the agency.
  * @param {Agency} agency An Agency object.
  */
-exports.directory = (agency) => `./geojson/${agency.key}`;
+exports.directory = (agency) => path.join(".", "geojson", agency.key);
+
+/**
+ * Return a file path for the agency and file type.
+ * @param {Agency} agency An Agency object.
+ * @param {String} type   The type of the file.
+ */
+exports.filepath = (agency, type) => path.join(this.directory(agency), `${agency.key}-${type}.geojson`);
 
 /**
  * Serialize service area data to a file in the data output directory.
  * @param {Agency} agency               An Agency object.
- * @param {String} type                 The type of the service area, used as a file suffix.
+ * @param {String} type                 The type of the service area.
  * @param {turf.FeatureCollection} data The GeoJSON data of the service area.
  */
 exports.data = (agency, type, data) => {
-    const filename = path.join(agency.directory, `${agency.key}-${type}.geojson`);
+    const filename = this.filepath(agency, type);
 
     fs.writeFileSync(filename, JSON.stringify(data));
 };
